9387a7ac707f534d75ab379c3f33e4d7d39c81c2
Alexandro B S <jornalsinal@gmail.com>
2025-03-16 12:40:17 -0300

    Meus estudos e códigos de 2025 Operadores Aritméticos, Relacionais e Lógicos

--- app/src/main/java/com/example/mystudycodes25/Operadores.kt
+++ app/src/main/java/com/example/mystudycodes25/Operadores.kt
@@ -29,4 +29,25 @@ fun main() {
     println(resultado6)
     println(resultado7)
 
+    //Operadores Relacionais
+    println(num1 == num2)
+    println(num1 != num2)
+    println(num1 > num2)
+    println(num1 < num2)
+    println(num1 >= num2)
+    println(num1 <= num2)
+
+    //Operadores Lógicos
+    println(num1 > num2 && num2 > num1)
+    println(num1 > num2 || num2 > num1)
+    var resultado8 = false && true
+    println(resultado8)
+    println(!resultado8)
+
+    val idadePessoa = 30
+    val comprasPessoa = 1000
+    val resultado9 = idadePessoa >= 50 && comprasPessoa >= 400
+    val resultado10 = idadePessoa >= 50 || comprasPessoa >= 400
+    println(resultado9)
+    println(resultado10)
 }
