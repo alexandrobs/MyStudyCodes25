cc102ec807c428184afd87b40f81f23eb095acbe
Alexandro B S <jornalsinal@gmail.com>
2025-03-24 18:54:25 -0300

    Meus estudos e códigos de 2025 Funções com e sem retorno inline

--- app/src/main/java/com/example/mystudycodes25/FuncoesRetorno.kt
+++ app/src/main/java/com/example/mystudycodes25/FuncoesRetorno.kt
@@ -13,9 +13,17 @@ fun somar2(): Int {
     return resultado
 }
 
+//funções inline, sem retorno
+fun subtrair() = println( 10 - 8)
+//funções inline, com retorno sem return, passando direto depois de =
+fun subtrair2(): Int = 10 - 8
+
 fun main() {
     somar()
     println(somar2())
     val retorno = somar2()
     println(retorno)
+
+    subtrair()
+    println(subtrair2())
 }
