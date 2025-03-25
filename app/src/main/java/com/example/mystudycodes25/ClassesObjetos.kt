8661462bdbd4fa5d133106fa7db05019367a64bc
Alexandro B S <jornalsinal@gmail.com>
2025-03-25 20:10:14 -0300

    Meus estudos e códigos de 2025 Classes, objetos, propriedades e métodos

---
+++ app/src/main/java/com/example/mystudycodes25/ClassesObjetos.kt
@@ -0,0 +1,33 @@
+package com.example.mystudycodes25
+
+class Jogador {
+    var kart = ""
+    var pneu = ""
+    var planador = ""
+
+    fun acelerar(aceleracao: Int = 0) {
+        if (aceleracao <= 0)
+        println("Acelerar de 0 a 80")
+        else if (aceleracao >= 80)
+            println("Acelerar de 80 ou mais que isso")
+        else
+            println("Sem acelerar")
+    }
+
+    fun retornarPoder(): String {
+        return "Poder do Casco Vermelho"
+    }
+
+    fun retornarEspecial(): String = "Soltando o Especial"
+}
+
+fun main() {
+    val jogador = Jogador()
+    jogador.kart = "Kart do Mario"
+    jogador.pneu = "Pneu de Asfalto"
+    jogador.planador = "Asa Delta"
+    jogador.acelerar(80)
+    jogador.acelerar()
+    println(jogador.retornarPoder())
+    println(jogador.retornarEspecial())
+}
