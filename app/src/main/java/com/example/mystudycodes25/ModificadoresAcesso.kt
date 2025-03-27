b5a301c8fa53df30d9af1afcbe0335fef695b994
Alexandro B S <jornalsinal@gmail.com>
2025-03-27 00:18:11 -0300

    Meus estudos e códigos de 2025 Modificadores de acesso - parte 2

--- app/src/main/java/com/example/mystudycodes25/ModificadoresAcesso.kt
+++ app/src/main/java/com/example/mystudycodes25/ModificadoresAcesso.kt
@@ -10,18 +10,55 @@ public
  */
 
 private class Carro2 {
-    var modelo = ""
-    fun acelerar() = println("Acelerar")
+    private var modelo = "Padrao"
+
+    private fun injetarCombustivel() {
+        println("Injetar combustivel")
+    }
+
+    fun acelerar() {
+        println("Exibe modelo do Carro2: $modelo")
+        injetarCombustivel()
+        println("Acelerar do Carro2")
+    }
 }
 
-class Carro1 {
-    var modelo = ""
-    fun acelerar() = println("Acelerar")
+//classe pai
+open class Carro1 {
+
+    protected open fun ligarModoTurbo() {
+        println("Modo turbo")
+    }
+
+    fun acelerar() {
+        ligarModoTurbo()
+        println("Acelerar do Carro1")
+    }
+}
+
+//classe filha
+class Gol : Carro1() {
+    override fun ligarModoTurbo() {
+        super.ligarModoTurbo()
+        println("do Gol ativado")
+    }
+}
+
+//classe filha
+class Ferrari : Carro1() {
+
 }
 
 fun main() {
     val carro = Carro1()
-    carro.modelo = "Esportivo"
-    println(carro.modelo)
     carro.acelerar()
+
+    val carro2 = Carro2()
+    carro2.acelerar()
+
+    val carro3 = Carro1()
+    carro3.acelerar()
+
+    val gol = Carro1()
+    gol.acelerar()
 }
