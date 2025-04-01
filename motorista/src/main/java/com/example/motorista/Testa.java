5ad22242a9dcb222f28775985af3443b4b744ed6
Alexandro B S <jornalsinal@gmail.com>
2025-04-01 19:07:40 -0300

    Meus estudos e códigos de 2025 String em java, usando substring

---
+++ motorista/src/main/java/com/example/motorista/Testa.java
@@ -0,0 +1,9 @@
+package com.example.motorista;
+
+public class Testa {
+
+    public static void main(String[] args) {
+        String s = "ALO MUNDO";
+        System.out.println(s.substring(4,6));
+    }
+}
