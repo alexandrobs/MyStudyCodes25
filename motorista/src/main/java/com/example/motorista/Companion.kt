0bf8563bceebcedc26cf40f80748092bbb17bd28
Alexandro B S <jornalsinal@gmail.com>
2025-03-31 23:28:30 -0300

    Meus estudos e códigos de 2025 Companion - parte 2

--- motorista/src/main/java/com/example/motorista/Companion.kt
+++ motorista/src/main/java/com/example/motorista/Companion.kt
@@ -4,12 +4,24 @@ class Carro(
     var modelo: String = "",
     var velocidade: Int = 0
 ) {
+
+    companion object {
+        const val VELOCIDADE_MAX_PERMITIDA = 120
+        fun exibirVelocidadeMaximaPermitida(){
+            println("Velocidade máxima permititda: ${Carro.VELOCIDADE_MAX_PERMITIDA}")
+        }
+    }
+
     fun exibirInformações() {
         println("Informações: $modelo e $velocidade")
     }
 }
 
 fun main() {
+
+    println("Velocidade máxima permititda: ${Carro.VELOCIDADE_MAX_PERMITIDA}")
+    Carro.exibirVelocidadeMaximaPermitida()
+
     val carro = Carro("Fusca", 100)
     carro.exibirInformações()
 
